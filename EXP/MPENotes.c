24fc7536a323d34da8099aa82d35e83f0a701d76
Stephen Duffy <duffysd@sunypoly.edu>
2026-02-05 21:26:06 -0500

    document MPI / cpilog example

--- EXP/MPENotes.c
+++ EXP/MPENotes.c
@@ -27,22 +27,16 @@ int main( int argc, char *argv[] )
 
     MPI_Init( &argc, &argv ); //initialize MPI passing the commandline args
 
-    MPI_Pcontrol( 0 );
+    MPI_Pcontrol( 0 );//set the profiling control level to 0 this value is sent to the profiling library
 
-    MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
-    MPI_Comm_rank( MPI_COMM_WORLD, &myid );
+    //determines the size of the group associated with the communicator.
+    MPI_Comm_size( MPI_COMM_WORLD, &numprocs );//get the number of parallel processes running
+    MPI_Comm_rank( MPI_COMM_WORLD, &myid );//get the id of this process
 
-    MPI_Get_processor_name( processor_name, &namelen );
-    fprintf( stderr, "Process %d running on %s\n", myid, processor_name );
+    MPI_Get_processor_name( processor_name, &namelen );//get the name of this host (useful if using many machines at once)
+    fprintf( stderr, "Process %d running on %s num procs: %d\n", myid, processor_name,numprocs );
 
-    /*
-        MPE_Init_log() & MPE_Finish_log() are NOT needed when
-        liblmpe.a is linked with this program.  In that case,
-        MPI_Init() would have called MPE_Init_log() already.
-    */
-#if defined( NO_MPI_LOGGING )
-#endif
-    MPE_Init_log();
+    MPE_Init_log();//initialize the logging for jumpshot
 
     /*
         user should NOT assign eventIDs directly in MPE_Describe_state()
@@ -50,12 +44,14 @@ int main( int argc, char *argv[] )
         MPE_Log_get_state_eventIDs() instead of the deprecated function
         MPE_Log_get_event_number().
     */
+    //create the event types
     MPE_Log_get_state_eventIDs( &event1a, &event1b );
     MPE_Log_get_state_eventIDs( &event2a, &event2b );
     MPE_Log_get_state_eventIDs( &event3a, &event3b );
     MPE_Log_get_state_eventIDs( &event4a, &event4b );
 
-    if ( myid == 0 ) {
+    if ( myid == 0 ) {//only do this on process 0
+        //set the name and color of each segment that gets displayed on jump shot
         MPE_Describe_state( event1a, event1b, "Broadcast", "red" );
         MPE_Describe_state( event2a, event2b, "Sync", "orange" );
         MPE_Describe_state( event3a, event3b, "Compute", "blue" );
@@ -63,40 +59,50 @@ int main( int argc, char *argv[] )
     }
 
     /* Get event ID for Solo-Event(single timestamp object) from MPE */
-    MPE_Log_get_solo_eventID( &event1 );
+    MPE_Log_get_solo_eventID( &event1 );//events that  only happen at specific instances
     MPE_Log_get_solo_eventID( &event2 );
     MPE_Log_get_solo_eventID( &event3 );
 
-    if ( myid == 0 ) {
+    if ( myid == 0 ) {//only on process 0
+        //set the names and colors for how each instant event will be displayed in jumpshot
        MPE_Describe_event( event1, "Broadcast Post", "white" );
        MPE_Describe_event( event2, "Compute Start", "purple" );
        MPE_Describe_event( event3, "Compute End", "navy" );
     }
 
-    if ( myid == 0 ) {
+    if ( myid == 0 ) {//on process 0
         n = 1000000;
-        startwtime = MPI_Wtime();
+        startwtime = MPI_Wtime();//get the elapsed time on the calling processor
     }
-    MPI_Barrier( MPI_COMM_WORLD );
+    MPI_Barrier( MPI_COMM_WORLD );//blocks on all processes until they all reach this point
 
-    MPI_Pcontrol( 1 );
-    MPE_Start_log();
+    MPI_Pcontrol( 1 );//set the profiling level to 1
+    MPE_Start_log();//start logging events for jump shot
     /*
     */
 
+    //who made this and made theese horrible desisions
     for ( jj = 0; jj < 5; jj++ ) {
-        MPE_Log_event( event1a, 0, NULL );
+        MPE_Log_event( event1a, 0, NULL );//start event 1a the null here might be a comment string
+        /**Broadcasts a message from the process with rank "root" to all other processes of the communicator
+         @param buffer A pointer to the message data
+         @param count the number of entries in the buffer
+         @param datatype The data type of the buffer (handle)
+         @param root The rank of the board cast root (the rank of the process that sends the message)
+         @param comm The communcator (handle) what "universe" the message is being sent in
+         */
         MPI_Bcast( &n, 1, MPI_INT, 0, MPI_COMM_WORLD );
-        MPE_Log_event( event1b, 0, NULL );
+        MPE_Log_event( event1b, 0, NULL );//mark end of event 1
 
-        MPE_Log_event( event1, 0, NULL );
+        MPE_Log_event( event1, 0, NULL );//mark instant event 1
 
-        MPE_Log_event( event2a, 0, NULL );
-        MPI_Barrier( MPI_COMM_WORLD );
-        MPE_Log_event( event2b, 0, NULL );
+        MPE_Log_event( event2a, 0, NULL );//mark start of event 2
+        MPI_Barrier( MPI_COMM_WORLD );//sync event
+        MPE_Log_event( event2b, 0, NULL );//mark end of event 2
 
-        MPE_Log_event( event2, 0, NULL );
-        MPE_Log_event( event3a, 0, NULL );
+        MPE_Log_event( event2, 0, NULL );//mark instant event 2
+        MPE_Log_event( event3a, 0, NULL );//mark start of event 3
+        //do math
         h   = 1.0 / (double) n;
         sum = 0.0;
         for ( ii = myid + 1; ii <= n; ii += numprocs ) {
@@ -104,29 +110,37 @@ int main( int argc, char *argv[] )
             sum += f(x);
         }
         mypi = h * sum;
-        MPE_Log_event( event3b, 0, NULL );
-        MPE_Log_event( event3, 0, NULL );
+        MPE_Log_event( event3b, 0, NULL );//mark end of event 3
+        MPE_Log_event( event3, 0, NULL );//mark instant event 3
 
         pi = 0.0;
-        MPE_Log_event( event4a, 0, NULL );
+        MPE_Log_event( event4a, 0, NULL );//mark start of event 4
+        /**Reduces values on all processes to a single value
+        @param sendbuf address of the buffer to send
+        @param recvbuf address of the buffer to store the result in
+        @param count number of elements in the send buffer
+        @param datatype data type of the elements in the send buffer (handle)
+        @param op The reduce opperation to preform (handle)
+        @param root The rank of the process the result will be deliverd to
+        @param comm The communcator (handle) what "universe" the message is being sent in
+         */
         MPI_Reduce( &mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD );
-        MPE_Log_event( event4b, 0, NULL );
+        MPE_Log_event( event4b, 0, NULL );//mark end of event 4
 
-        MPE_Log_sync_clocks();
+        MPE_Log_sync_clocks();//sync all the clocks for the processes
     }
     if ( argv != NULL )
-        MPE_Finish_log( argv[0] );
+        MPE_Finish_log( argv[0] );//save the log file to the file specified in arg 0 ... so just the program name? when would argv be null?????
     else
         MPE_Finish_log( "cpilog" );
-#if defined( NO_MPI_LOGGING )
-#endif
 
-    if ( myid == 0 ) {
-        endwtime = MPI_Wtime();
+
+    if ( myid == 0 ) {//if process 0
+        endwtime = MPI_Wtime();//save the end time
         printf( "pi is approximately %.16f, Error is %.16f\n",
                 pi, fabs(pi - PI25DT) );
         printf( "wall clock time = %f\n", endwtime-startwtime );
     }
-    MPI_Finalize();
+    MPI_Finalize();//terminate the MPI execution enviorment
     return( 0 );
 }
